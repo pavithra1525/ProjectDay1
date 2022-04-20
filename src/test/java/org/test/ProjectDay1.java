469ee35b756306ca358ca95268e4f34d9f5421eb
pavithra1525 <pavikutty0925@gmail.com>
2022-04-20 17:25:24 +0530

    rest

--- src/test/java/org/test/ProjectDay1.java
+++ src/test/java/org/test/ProjectDay1.java
@@ -2,8 +2,8 @@ package org.test;
 
 public class ProjectDay1 {
 	public static void main(String[] args) {
+		System.out.println("muthu");
 		System.out.println("sathya");
-		System.out.println("nithya");
 	}
 
 }
