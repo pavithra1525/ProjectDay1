839f39c83aef22a87dd4dc370a4cb3c9ca308d25
pavithra1525 <pavikutty0925@gmail.com>
2022-04-20 16:52:32 +0530

    reslove

--- src/test/java/org/test/ProjectDay1.java
+++ src/test/java/org/test/ProjectDay1.java
@@ -2,8 +2,8 @@ package org.test;
 
 public class ProjectDay1 {
 	public static void main(String[] args) {
-		System.out.println("pavi");
-		System.out.println("chithra");
+		System.out.println("muthu");
+		System.out.println("sathya");
 	}
 
 }
